3fa332cfbc43538d7d9f2c61bbc928094d797e55
Debaprakash Jena <debaprakashjena2021@gmail.com>
2026-02-12 10:52:34 +0530

    slightly updated

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -6,10 +6,10 @@ router.post('/test', (req, res) => {
     console.log("🔥Received webhook Recieved");
     const event = req.body;
     res.status(200).json({
-         success: true, 
          received:true
         }
     );
+
 
 });
 
